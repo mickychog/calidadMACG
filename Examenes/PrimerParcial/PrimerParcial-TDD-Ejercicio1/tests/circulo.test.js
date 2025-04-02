1288c6b67591971466d5982df9663040c75a3584
Miguel <mickychog@gmail.com>
2025-04-02 07:52:10 -0400

    Caso de prueba 2: Paso

--- Examenes/PrimerParcial/PrimerParcial-TDD-Ejercicio1/tests/circulo.test.js
+++ Examenes/PrimerParcial/PrimerParcial-TDD-Ejercicio1/tests/circulo.test.js
@@ -4,3 +4,8 @@ test('Calcula el area con radio positivo r=5', () => {
     expect(areaCirculo(5)).toBeCloseTo(78.5398, 4);
 });
 
+test('Calcula el area con radio cero r=0', () => {
+    expect(areaCirculo(0)).toBe(0);
+});
+
+
